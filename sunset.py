688e99ebc251c191d6f9f8a5f8842564748eb3ed
ksg-dev <ksg.dev.data@gmail.com>
2024-06-14 16:30:56 -0500

    isolate hour for sunset, sunrise, and now

--- sunset.py
+++ sunset.py
@@ -21,9 +21,8 @@ sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
 sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
 
-
 print(sunrise)
 print(sunset)
 
-# time_now = datetime.now()
-# print(time_now)
+time_now = datetime.now()
+print(time_now.hour)
